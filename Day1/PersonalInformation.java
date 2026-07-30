303ac0ddbeda2f52b76ebad8b2a59c02db6694e4
anandxjha <jhaanand1105@gmail.com>
2026-07-30 22:27:18 +0530

    Day 1

---
+++ Day1/PersonalInformation.java
@@ -0,0 +1,27 @@
+package Day1;
+import java.util.*;
+import java.lang.*;
+
+public class PersonalInformation {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = sc.nextLine();
+        System.out.print("Enter your Age: ");
+        int age = sc.nextInt();
+        sc.nextLine();
+        System.out.print("Enter your City: ");
+        String city = sc.nextLine();
+
+        System.out.println("-------------------");
+        System.out.println("Perional Details");
+        System.out.println("-------------------");
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("City: " + city);
+
+
+    }
+
+}
---
+++ Day1/RectangleArea.java
@@ -0,0 +1,15 @@
+package Day1;
+import java.util.*;
+public class RectangleArea {
+    public static void main(String[]args){
+        Scanner sc= new Scanner (System.in);
+        System.out.print("Enter the length: ");
+        float length = sc.nextFloat();
+
+        System.out.print("Enter the Width: ");
+        float width = sc.nextFloat();
+
+        Float area = length * width;
+        System.out.println("Area of Rectangle = " +area);
+    }
+}
