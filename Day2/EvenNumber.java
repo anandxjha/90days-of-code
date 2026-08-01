eb8f73d7ca3900b0250978e826aa001d7c8a1ee4
anandxjha <jhaanand1105@gmail.com>
2026-08-01 23:19:15 +0530

    day3

--- Day2/EvenNumber.java
+++ Day2/EvenNumber.java
@@ -1,4 +1,12 @@
 package Day2;
-
+import java.util.*;
 public class EvenNumber {
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the Number: ");
+        int number= sc.nextInt();
+
+    }
+
+
 }
