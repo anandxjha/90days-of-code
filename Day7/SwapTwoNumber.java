b8186528f7846c19c2680040ed735b3b4a4f1b94
anandxjha <jhaanand1105@gmail.com>
2026-08-05 21:53:10 +0530

    Day7

---
+++ Day7/SwapTwoNumber.java
@@ -0,0 +1,21 @@
+package Day7;
+
+import java.util.Scanner;
+
+public class SwapTwoNumber {
+
+    public static void main(String []args){
+        Scanner sc=new Scanner (System.in);
+        System.out.print("Enter first number :");
+        int a = sc.nextInt();
+        System.out.print("Enter second number :");
+        int b= sc.nextInt();
+
+         a=a+b;
+         b=a-b;
+         a=a-b;
+        System.out.println( "After Swapping "+ a);
+        System.out.println("After Swapping" +b);
+
+    }
+}
