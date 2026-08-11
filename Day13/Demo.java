32ae75e2a59be43c35556fe9f6a6a0210ee34879
anandxjha <jhaanand1105@gmail.com>
2026-08-11 21:22:33 +0530

    Day13

---
+++ Day13/Demo.java
@@ -0,0 +1,7 @@
+package Day13;
+
+public class Demo {
+    static void main() {
+        System.out.println("Demo");
+    }
+}
---
+++ Day7/WhileLoop.java
@@ -0,0 +1,15 @@
+package Day7;
+
+public class WhileLoop {
+}
+// while loops |
+
+/*
+Syntax:
+    while (condition) {
+
+          // body
+
+        }
+
+        */
