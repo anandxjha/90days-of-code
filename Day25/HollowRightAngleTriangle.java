8c8467695f3658efe5ffb5a39056e33c28e2fafb
anandxjha <jhaanand1105@gmail.com>
2026-08-24 22:25:28 +0530

    Day25

--- Day25/HollowRightAngleTriangle.java
+++ Day25/HollowRightAngleTriangle.java
@@ -2,13 +2,22 @@ package Day25;
 
 public class HollowRightAngleTriangle {
     static void main() {
-        int n=5;
-        for(int row=1;row<=n;row++){
-            for(int col=1;col<=n;col++){
-                if(row ==1 || row ==2 || row==n) {
-                    System.out.println("* ");
+        int n = 5;
+        for (int row = 1; row <= n; row++) {
+            if (row == 1 || row == 2 || row == n) {
+                for (int col = 1; col <= row; col++) {
+                    System.out.print("* ");
                 }
             }
+            else {
+                System.out.print("* ");
+
+                for (int col = 1; col <= (row - 2); col++) {
+                    System.out.print(" ");
+                }
+                System.out.print("* ");
+            }
+                            System.out.println();
         }
     }
 }
