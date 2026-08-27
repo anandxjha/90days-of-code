8f17126247b9f6a2966181e36b01bf87d6cf688f
anandxjha <jhaanand1105@gmail.com>
2026-08-27 22:23:31 +0530

    Day29

---
+++ Day29/Method.java
@@ -0,0 +1,12 @@
+package Day29;
+
+public class Method {
+    static void name(int a,int b){  //yaha int a or int b ko parameter kehte hai
+        System.out.println(a+b);
+    }
+
+
+    static void main() {
+        name(10,20);
+    }
+}
