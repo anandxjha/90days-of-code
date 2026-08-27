d40606ff884aa01cf6a6493ddc1bc65fa5fd02d6
anandxjha <jhaanand1105@gmail.com>
2026-08-27 22:45:05 +0530

    Day29

--- Day29/Method.java
+++ Day29/Method.java
@@ -1,12 +1,23 @@
 package Day29;
 
 public class Method {
-    static void name(int a,int b){  //yaha int a or int b ko parameter kehte hai
-        System.out.println(a+b);
+//    static void name(int a,int b){  ///yaha int a or int b ko parameter kehte hai
+//        System.out.println(a+b);
+//    }
+//
+//
+//    static void main() {
+//        name(10,20); ///jb hm method ko call krte hai to yaha 10,20  actual value hai jinhe argument kete hai
+//    }
+    static void sayName(String name){
+        System.out.println(name);
+
     }
 
-
     static void main() {
-        name(10,20); //jb hm method ko call krte hai to yaha 10,20  actual value hai jinhe argument kete hai
+        sayName("Anand");
+        sayName("Aman");
+        sayName("Shubham");
+
     }
 }
